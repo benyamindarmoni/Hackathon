05e4ddca14075f57988b574e1fe841f4ddbc81e4
unknown <aviad198@gmail.com>
2019-05-19 19:21:27 +0300

    schedual and Ctime

---
+++ hack/Schedule.java
@@ -0,0 +1,25 @@
+package hack;
+
+public class Schedule {
+int grade;
+Course mat[][];
+static final Course ILUZCOMP = new Course();
+enum FailCuz {
+	SUCC, TAKEN, ILUZ
+}
+
+public Schedule() {
+	mat = new Course[28][6];
+	grade = 0;
+}
+
+public int insert(Course cor) {
+	
+	if()
+	return false;
+}
+
+
+
+
+}
