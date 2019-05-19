a6d7e41ae0c5e6f069e7d6310f9cfa099f663d11
IgorKrol <igork08@gmail.com>
2019-05-19 23:32:07 +0300

    asd

--- Constraints.java
+++ Constraints.java
@@ -3,7 +3,14 @@ package hack;
 import java.util.ArrayList;
 
 public class Constraints {
-	ArrayList<Ctime> Constraint =new ArrayList<Ctime>();
+	ArrayList<Ctime> constraint;
 	
+	public Constraints() {
+		constraint = new ArrayList<Ctime>();
+	}
+	/* Add new Constraint to collection */
+	public void AddConstraint(Ctime c) {
+		constraint.add(c);
+	}
 
 }
--- Ctime.java
+++ Ctime.java
@@ -9,16 +9,15 @@ public class Ctime {
 	public Ctime(String t) {
 		
 	}
-<<<<<<< HEAD
+
 public Ctime(Ctime t) {
 		day=t.day;
 		start=t.start;
 		duration=t.duration;
 	}
-	public Ctime(int _day, int _start, int _duration) {
-=======
+
 	public Ctime(int _day, double _start, double _duration) {
->>>>>>> 922b0f91c67290a437ba2c19dd6e39e6bda524a1
+
 		day = _day;
 		start = _start;
 		duration = _duration;
@@ -27,15 +26,13 @@ public Ctime(Ctime t) {
 	public int getDay() {
 		return day;
 	}
-<<<<<<< HEAD
+
 	public boolean equals(Ctime a) {
 		if(day==a.day&&start==a.start&&duration==a.duration)return true;
 		return false;
 	}
-	public int getStart() {
-=======
+	
 	public double getStart() {
->>>>>>> 922b0f91c67290a437ba2c19dd6e39e6bda524a1
 		return start;
 	}
 	public double getDuration() {
