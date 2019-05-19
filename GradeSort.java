1cb7751fdc75b09be999211379a8754c5957fd56
IgorKrol <igork08@gmail.com>
2019-05-20 02:21:33 +0300

    gradesort start

---
+++ GradeSort.java
@@ -0,0 +1,15 @@
+package hack;
+
+public class GradeSort {
+	
+	ScheduleContainer sc;
+	
+	public GradeSort(ScheduleContainer _sc) {
+		sc = _sc;
+	}
+	
+	public void sortByGrades() {
+	
+	}
+	
+}
--- ScheduleContainer.java
+++ ScheduleContainer.java
@@ -3,5 +3,18 @@ package hack;
 import java.util.ArrayList;
 
 public class ScheduleContainer {
-	ArrayList<Schedule> options=new ArrayList<Schedule>();
+	ArrayList<Schedule> scheduleList;
+	
+	public ScheduleContainer() {
+		scheduleList =new ArrayList<Schedule>();
+	}
+	
+	public void AddSchedule(Schedule s) {
+		scheduleList.add(s);
+	}
+	
+	public ArrayList<Schedule> getList() {
+		return scheduleList;
+	}
+	
 }
