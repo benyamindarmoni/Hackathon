bebaa5379e1e39747cfa06eefe625d3d5cd5d1d1
unknown <aviad198@gmail.com>
2019-05-19 22:14:14 +0300

    SAdsad

--- Schedule.java
+++ Schedule.java
@@ -7,28 +7,24 @@ public class Schedule {
 		mat = new Course[28][6];
 		grade = 0;
 	}
-	
-	public int[] timeToIndex(Ctime t) {
-		int[] index = new int[2];
-		index[1] = t.getDay();	// j = day
-		index[0] = t.getStart() - 9 * 2; //start = hour, 
-		
-	}
+
 
 	public boolean insert(Course cor) {
-	int[] index = timeToIndex(cor.getCtime());	//index = [i,j] for cor start time.
+		double[] index = timeToIndex(cor.getCtime());	//index = [i,j] for cor start time.
 		if (mat[index[0]][index[1]] == null)//the 
 		{
 			for (double i = cor.getCtime().getDuration(); i>=0;i=i-0.5) {
-				
+				mat[index[0]][i]=Course;
 			}
+			return true;
 		}
+		else  
+			return false;
+
 	}
-
-
 }
 
 
 
 
-}
+
