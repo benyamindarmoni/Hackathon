bbffb2e24b747a2d5f688a278e17eecd7c4aa44b
benyamindarmoni <benyamindarmoni@gmail.com>
2019-05-20 03:14:08 +0300

    sinun

--- Algorithm.java
+++ Algorithm.java
@@ -4,31 +4,45 @@ import java.util.ArrayList;
 
 
 public class Algorithm {
-Course availble[][];
+	ArrayList<ArrayList<Course>> availble;//availble.get(i) = main courses, 
+	//availble.get(i).get(j) = sub times.
+	
 
-
+	public Algorithm() {
+		availble = new ArrayList<ArrayList<Course>>();
+	}
 	public void sinun(Course before[],Constraints c ) {
-		availble =new Course [before.length][before.length];
+
 		boolean good=true;
 		for(int i=0;i<before.length;i++) {
-			for(int j=0;j<c.Constraint.size();j++) {
-				if(availble[0][i].getTime().doesCollide(c.Constraint.get(j))) {
+			for(int j=0;j<c.constraint.size();j++) {
+				if(availble.get(i).get(j).getTime().doesCollide(c.constraint.get(j))) {
 					good=false;
 					break;
 				}
-				if(j+1==c.Constraint.size()&&good) {
+				if(j+1==c.constraint.size()&&good) {
 					//need to add a[i] 2 case: name of course a[i] is inside or not.
+					boolean found=false;
+					for(int x=0;x<availble.size();x++) {
+						if(availble.get(x).get(0).getCourse_Name().equals(before[i].getCourse_Name())) {
+							availble.get(x).add(before[i]);
+							found=true;
+							break;
+						}
+					}
+					if(!found) {
+						availble.add(new ArrayList<Course>());
+						availble.get(availble.size()-1).add(before[i]);
+					}
 
-					available.get(index)
+
 				}
-
-
+				good=true;
 			}
-			good=true;
 		}
 	}
 	public void build_scheduls() {
-		
+
 	}
 
 
